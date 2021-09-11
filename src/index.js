ee82a68266b636fb8bda37726aa16f68d46393e7
Vicente Gutierrez <gtz.rvera@gmail.com>
2021-09-10 23:46:38 -0500

    Login to todoist page

--- src/index.js
+++ src/index.js
@@ -14,11 +14,31 @@ const { getTaskInfo } = require('../helpers/tasks');
       waitUntil: 'networkidle2'
     });
 
+    await page.waitForXPath('//div[@class="tasks-card-container row"]');
     const taskContainers = await page.$x('//div[@class="taskCard card"]//div[@class="card-body"]');
     const tasks = await Promise.all(taskContainers.map(task => getTaskInfo(task, page)));
     
-    console.log(tasks);
+    await page.goto(process.env.TODOIST_URL, {
+      waitUntil: 'networkidle2'
+    });
 
+    await page.waitForXPath('//header//a[contains(text(), "Iniciar sesión")]');
+    const [visibleLoginLink] = await page.$x('//header//a[contains(text(), "Iniciar sesión")]');
+    
+    await visibleLoginLink.click();
+
+    await Promise.all([
+      page.waitForSelector('#email'),
+      page.waitForSelector('#password')
+    ]);
+
+    await page.type('#email', process.env.EMAIL);
+    await page.type('#password', process.env.PASSWORD);
+
+    const [loginButton] = await page.$x('//button[contains(text(), "Inicia sesión")]');
+    await loginButton.click();
+
+    await page.waitForSelector('#quick_add_task_holder');
   } catch (e) {
     console.error(e);
   }
